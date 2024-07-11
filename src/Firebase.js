04975a4a32eb2e85ab0b75df19fc3b04e062644a
abbas hassan <abbass.hassan.a7@gmail.com>
2024-07-11 12:51:29 +0300

    setup firebase and tailwind

---
+++ src/Firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzSLD1C8d4jAuzOmCdeB9ZHI0HgviBY3g",
+  authDomain: "scanner-kb816f.firebaseapp.com",
+  projectId: "scanner-kb816f",
+  storageBucket: "scanner-kb816f.appspot.com",
+  messagingSenderId: "202007813558",
+  appId: "1:202007813558:web:355df7fb4eaf8254bbb517",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
