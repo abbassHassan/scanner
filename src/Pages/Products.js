0b9645b8f2e00c07c7b909ddb2b4272bb5ca879b
abbas hassan <abbass.hassan.a7@gmail.com>
2024-07-18 10:56:40 +0300

    nth

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -50,7 +50,7 @@ const Products = () => {
           className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
           onClick={handleAddItem}
         >
-          Add Item
+          Add Product
         </button>
       </div>
       {showForm && <AddProduct onAdd={handleAddProduct} />}
